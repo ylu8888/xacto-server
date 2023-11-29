981d324245f1e040b26386e469a3472cdc95f224
ylu <123523291+ylu8888@users.noreply.github.com>
2023-11-29 17:01:05 -0500

    small fixes to send packet

--- protocol.c
+++ protocol.c
@@ -1,18 +1,21 @@
-int proto_send_packet(int fd, XACTO_HEADER *pkt, void *data){
+#include "protocol.h"
+#include "csapp.h"
+
+int proto_send_packet(int fd, XACTO_PACKET *pkt, void *data){
   //first convert the multi-byte fields in packet from normal host byte order to network byte order
   pkt->serial = htonl(pkt->serial);
   pkt->size = htonl(pkt->size);
   pkt->timestamp_sec = htonl(pkt->timestamp_sec);
-  pkt->timestamp_nsec = htonl(pkt->timestamp_nsec);
+  pkt->timestamp_nsec = htonl(pkt->timestamp_nsec); //htonl is host long to network
    
-  //then write the header to the network connection
+  //then write the packet header to the network connection
   int writeRez = write(fd, pkt, sizeof(XACTO_PACKET));
   if(writeRez < 0) return -1; //error handler
 
-  //if length of header is not zero, call write again to write payload data to network conn
+  //if length of header is not zero, call write again to write payload data to network connection
 
-  if(data != NULL && pkt->length != 0){
-    int writeRes = write(fd, data, nhtol(pkt->length)); //nhtol is network to host long
+  if(data != NULL && pkt->size != 0){
+    int writeRes = write(fd, data, ntohl(pkt->size)); //nhtol is network to host long
 
     if(writeRes < 0) return -1;
   }
@@ -20,6 +23,6 @@ int proto_send_packet(int fd, XACTO_HEADER *pkt, void *data){
   return 0;
 }
 
-int proto_recv_packet(int fd, XACTO_PACKET *pkt, void **datap){
+// int proto_recv_packet(int fd, XACTO_PACKET *pkt, void **datap){
   
-}
+// }

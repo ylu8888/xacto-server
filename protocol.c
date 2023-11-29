f277dfe1fe986860f9f4e162a6d1e2cffb256175
ylu <123523291+ylu8888@users.noreply.github.com>
2023-11-29 17:16:13 -0500

    fin first func i think

--- protocol.c
+++ protocol.c
@@ -9,13 +9,12 @@ int proto_send_packet(int fd, XACTO_PACKET *pkt, void *data){
   pkt->timestamp_nsec = htonl(pkt->timestamp_nsec); //htonl is host long to network
    
   //then write the packet header to the network connection
-  int writeRez = write(fd, pkt, sizeof(XACTO_PACKET));
+  int writeRez = write(fd, pkt, sizeof(pkt));
   if(writeRez < 0) return -1; //error handler
 
   //if length of header is not zero, call write again to write payload data to network connection
-
   if(data != NULL && pkt->size != 0){
-    int writeRes = write(fd, data, ntohl(pkt->size)); //nhtol is network to host long
+    int writeRes = write(fd, data, htonl(pkt->size)); //nhtol is network to host long
 
     if(writeRes < 0) return -1;
   }

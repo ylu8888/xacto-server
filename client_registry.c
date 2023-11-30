cc82a86c13fbaea8efb6ea62afc00f22db26cbf6
ylu <123523291+ylu8888@users.noreply.github.com>
2023-11-30 18:04:51 -0500

    fin register

--- client_registry.c
+++ client_registry.c
@@ -1,5 +1,6 @@
 #include "csapp.h"
 #include "client_registry.h"
+#include "semaphore.h"
 
 int max = 3000;
 
@@ -15,11 +16,11 @@ typedef struct client_node {
 } CLIENT_NODE;
 
 CLIENT_REGISTRY *creg_init(){
-  CLIENT_REGISTRY client = malloc(sizeof(CLIENT_REGISTRY)); 
+  CLIENT_REGISTRY client = Malloc(sizeof(CLIENT_REGISTRY)); 
   if(client == NULL) return NULL; //error case
 
   cr->clients = NULL; //initialize the fields of the client registry
-  sem_init(&cr->mutex,0 ,1);
+  sem_init(&cr->mutex, 0, 1);
 
   return cr;
 }
@@ -36,4 +37,29 @@ void creg_fini(CLIENT_REGISTRY *cr){
 
     free(cr);
   
+}
+
+int creg_register(CLIENT_REGISTRY *cr, int fd){
+    CLIENT_NODE* node = malloc(sizeof(CLIENT_NODE)); //create the new node
+    if(node == NULL) return -1; //error check
+
+    sem_wait(&cr->mutex);
+
+    node->fd = fd; //assign the fields of the new node 
+    node->next = NULL;
+
+    if(cr->clients == NULL){ //if the linked list is empty, set the head of linklist equal to the new node
+        cr->clients = node;
+    } 
+    else{
+        CLIENT_NODE* curr = cr->clients;
+        while(curr.next != NULL){ //need to do curr.next in order to actually append to end of link list
+            curr = curr.next;
+        }
+        curr.next = node;
+    }
+
+    sem_post(&cr->mutex);
+
+    return 0;
 }

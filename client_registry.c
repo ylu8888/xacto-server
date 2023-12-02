3bde08ab169e54462b5b33d3cc9addc59d8aa834
ylu <123523291+ylu8888@users.noreply.github.com>
2023-12-02 13:27:22 -0500

    fin unregister, wait, and sd

--- client_registry.c
+++ client_registry.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "client_registry.h"
+#include "debug.h"
+#include "server.h"
 
 int max = 3000;
 
@@ -13,6 +15,7 @@ typedef struct CLIENT_NODE {
 
 typedef struct client_registry {
     sem_t mutex;
+    sem_t empty;
     CLIENT_NODE* clients; //linked list to store the clients because it dynamically expands, easier than array
 
 } CLIENT_REGISTRY;
@@ -23,6 +26,7 @@ CLIENT_REGISTRY *creg_init(){
 
   client->clients = NULL; //initialize the fields of the client registry
   sem_init(&client->mutex, 0, 1);
+  sem_init(&client->empty, 0, 0); //initial value of 0 to indicate locked
 
   return client;
 }
@@ -41,11 +45,11 @@ void creg_fini(CLIENT_REGISTRY *cr){
   
 }
 
-int creg_register(CLIENT_REGISTRY *cr, int fd){
+int creg_register(CLIENT_REGISTRY *cr, int fd){ //adding new client to clientlist
     CLIENT_NODE* node = malloc(sizeof(CLIENT_NODE)); //create the new node
     if(node == NULL) return -1; //error check
 
-    sem_wait(&cr->mutex);
+    sem_wait(&cr->mutex); //wait for semaphore to become available
 
     node->fd = fd; //assign the fields of the new node 
     node->next = NULL;
@@ -61,7 +65,84 @@ int creg_register(CLIENT_REGISTRY *cr, int fd){
         curr->next = node;
     }
 
-    sem_post(&cr->mutex);
+    sem_post(&cr->mutex); //post after semaphore is finished waiting and unblocked
 
     return 0;
+}
+
+int creg_unregister(CLIENT_REGISTRY *cr, int fd){
+    //remove client from clientlist
+    CLIENT_NODE* curr = cr->clients;
+    CLIENT_NODE* prev = NULL;
+    sem_wait(&cr->mutex);
+
+    if(curr == NULL){
+        sem_post(&cr->mutex);
+        return -1; //the head of Linklist is null, client registry not set yet
+    }
+
+    if(curr->fd == fd){ //if the head is the client to remove
+        cr->clients =  curr->next;
+        free(curr); //delete the head
+        
+
+        if(cr->clients == NULL){ //link list is now empty after removing 
+            sem_post(&cr->empty);
+        }
+
+        sem_post(&cr->mutex);
+        return 0;
+
+    }
+
+    while(curr != NULL){
+        if(curr->fd == fd){
+            prev->next = curr->next; //set the prev equal to currs next, removing it from linklist
+            free(curr); //delete client node
+
+            if(cr->clients == NULL){ //link list is now empty after removing 
+            sem_post(&cr->empty);
+             }
+
+             sem_post(&cr->mutex);
+            return 0;
+
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    sem_post(&cr->mutex);
+    return -1; //error if the client was not found in the list
+}
+
+void creg_wait_for_empty(CLIENT_REGISTRY *cr){
+    for(;;){
+        sem_wait(&cr->mutex);
+        
+        if (cr->clients == NULL) {
+            sem_post(&cr->mutex);
+            return;
+
+        }
+
+        sem_post(&cr->mutex);
+
+    }
+    
+}
+
+void creg_shutdown_all(CLIENT_REGISTRY *cr){
+    //shutdown all socket connections for registered clients
+
+    sem_wait(&cr->mutex);
+
+    CLIENT_NODE* curr = cr->clients;
+    while(curr != NULL){
+        shutdown(curr->fd, SHUT_RDWR);
+        curr = curr->next;
+    }
+
+    sem_post(&cr->mutex);
+    
 }

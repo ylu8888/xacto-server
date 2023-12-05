208901073e170325762422a6329772633a4cbfe0
ylu <123523291+ylu8888@users.noreply.github.com>
2023-12-05 17:07:29 -0500

    DONE WITH PUT MAYBE

--- server.c
+++ server.c
@@ -37,8 +37,6 @@ void *xacto_client_service(void *arg){
 		break; //error
 	     }
 
-	    uint32_t serial = reqpkt->serial;
-
 	     if(reqpkt->type == XACTO_PUT_PKT){
 
 		    //this gets the KEY, stored in datak
@@ -51,9 +49,9 @@ void *xacto_client_service(void *arg){
 			break; //error
 		     }
 
-		  //   create blob with a size of recieved->size (ntohl) and key_store ptr
-		     BLOB* blobVal = (datak, ntohl(reqpkt->size));
-		     KEY* tempKey = (blobVal);
+		  // create blob with a size of recieved->size (ntohl) and key_store ptr
+		     BLOB* blobVal = blob_create(datak, ntohl(reqpkt->size));
+		     KEY* tempKey = key_create(blobVal);
 		  // key ptr = key create(just created blob)
 
 		     TRANS_STATUS tstat = store_put(trans, tempKey, blobVal); //put a key/value mapping in store
@@ -62,9 +60,9 @@ void *xacto_client_service(void *arg){
 		     XACTO_PACKET *reppkt = malloc(sizeof(XACTO_PACKET)); //make a xacto packet with type reply
 		     reppkt->type = XACTO_REPLY_PKT; //initialize the xacto packet struct for REPLY
 		     reppkt->status = tstat; //the output of STORE is a status, send that in as a reppkt->status
-		     reppkt->serial = serial; //serial number is gotten from the first read
+		     reppkt->serial = reqpkt->serial; //serial number is gotten from the first read
 		     reppkt->null = 1;  //rest is 0
-		     reppkt->size = 0;
+		     reppkt->size = 0; //the size is 0 because we're not sending any payloads
 		     reppkt->timestamp_sec = 0;
 		     reppkt->timestamp_nsec = 0;
 
@@ -87,23 +85,51 @@ void *xacto_client_service(void *arg){
 			break; //error
 		     }
 
-		     (BLOB*) valBlob;
+		     BLOB* valBlob = NULL; //stores thre value
+
+		     //blobVal is a BLOB for getting the KEY
+		     //valBlob is a BLOB for getting the VALUE
+		     BLOB* blobVal = blob_create(datak, ntohl(reqpkt->size)); //blob with keyptr 
+		     KEY* tempKey = key_create(blobVal); //creating a key
+		     
 		     //the value from store_get is stored inside of datav, as per the third argument &datav.
-		     TRANS_STATUS gstat = store_get(trans, (KEY*) datak, &valBlob); //put a key/value mapping in store
+		     TRANS_STATUS gstat = store_get(trans, tempKey, &valBlob); //put a key/value mapping in store
+
+		     BLOB* newVal = blob_create(valBlob->content, sizeof(*valBlob));
 		     
 		     //use proto_send_pkt for REPLY after key and value
 		     XACTO_PACKET *reppkt = malloc(sizeof(XACTO_PACKET)); //make a xacto packet with type reply
 		     reppkt->type = XACTO_REPLY_PKT; //initialize the xacto packet struct for REPLY
 		     reppkt->status = gstat; //the output of STORE is a status, send that in as a reppkt->status
-		     reppkt->serial = serial; //serial number is gotten from the first read
+		     reppkt->serial = reqpkt->serial; //serial number is gotten from the first read
 		     reppkt->null = 0;  //rest is 0
 		     reppkt->size = 0;
 		     reppkt->timestamp_sec = 0;
 		     reppkt->timestamp_nsec = 0;
 
-		    // void* datax = NULL;
+		     //THIS IS THE DATA PACKET, ABOVE IS THE REPLY PACKET
+		     XACTO_PACKET *datapkt = malloc(sizeof(XACTO_PACKET)); 
+		     datapkt->type = XACTO_VALUE_PKT;
+		     datapkt->status = gstat; 
+		     datapkt->serial = reqpkt->serial;
+		     if(valBlob->content == NULL){
+			datapkt->null = 1;
+		     } else{
+			datapkt->null = 0;
+		     }
+		     datapkt->size = sizeof(*valBlob);
+		     datapkt->timestamp_sec = 0;
+		     datapkt->timestamp_nsec = 0;
+		     
+		    //WE SEND IN TWO PACKETS: REPLY AND DATA PCKET
+		     //for the third argument void* data: send in NULL for the reply and value Blob for the data
+		     void* datax = NULL;
 		     //we want to send in the VALUE from GET in the packet, which is stored in DATAV
-		     if(proto_send_packet(connfd, reppkt, valBlob) == -1){ //send after making REPLY packet
+		     if(proto_send_packet(connfd, reppkt, datax) == -1){ //send after making REPLY packet
+			break; //error
+		     }
+
+		     if(proto_send_packet(connfd, datapkt, valBlob) == -1){ //send after making REPLY packet
 			break; //error
 		     }
 
@@ -115,13 +141,13 @@ void *xacto_client_service(void *arg){
 		   
 	     }
 	     else if(reqpkt->type == XACTO_COMMIT_PKT){
-		TRANS_STATUS cstat = trans_commit(trans);
+	     TRANS_STATUS cstat = trans_commit(trans);
 
 	     XACTO_PACKET *reppkt = malloc(sizeof(XACTO_PACKET)); //make a xacto packet with type reply
 	     reppkt->type = XACTO_REPLY_PKT; //initialize the xacto packet struct for REPLY
 	     reppkt->status = cstat; //the output of STORE is a status, send that in as a reppkt->status
-	     reppkt->serial = serial; //serial number is gotten from the first read
-	     reppkt->null = 1;  //rest is 0
+	     reppkt->serial = reqpkt->serial; //serial number is gotten from the first read
+	     reppkt->null = 0;  //rest is 0
 	     reppkt->size = 0;
 	     reppkt->timestamp_sec = 0;
 	     reppkt->timestamp_nsec = 0;
